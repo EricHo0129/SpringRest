f7c2867d507785349a62d16221380ab7540aece1
yung.ho <yung.ho@YUNG_HO_T470.e104.com.tw>
2017-09-08 17:51:59 +0800

    add delete method

--- src/main/java/com/eric/spring/controller/ProfileRestController.java
+++ src/main/java/com/eric/spring/controller/ProfileRestController.java
@@ -8,7 +8,11 @@ package com.eric.spring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eric.spring.dao.ProfileDAO;
@@ -20,8 +24,25 @@ public class ProfileRestController {
 	@Autowired
 	private ProfileDAO profileDAO;
 	
-	@GetMapping("/profileList")
-	public List<ProfileModel> getProfileList() {
+	/*這種寫法無法控制回傳status,但吐出去一樣是JSON*/
+	@GetMapping("/profiles")
+	public List<ProfileModel> getProfiles() {
 		return profileDAO.queryAll();
+	}
+	
+	@GetMapping("/profileList")
+	public ResponseEntity<List<ProfileModel>> getProfileList() {
+		return new ResponseEntity<List<ProfileModel>>(profileDAO.queryAll(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/profile/{pid}")
+	public ResponseEntity<ProfileModel> getProfile(@PathVariable("pid") Long pid) {
+		//return profileDAO.queryOne(pid);
+		return new ResponseEntity<ProfileModel>(profileDAO.queryOne(pid), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/profile/{pid}")
+	public ResponseEntity<Boolean> deleteProfile(@PathVariable("pid") Long pid) {
+		return new ResponseEntity<Boolean>( profileDAO.delete(pid), HttpStatus.OK);
 	}
 }
